f1b6a2f4271675a743705cfd84274f854f33f6c0
Waphey <awapheywork@gmail.com>
2025-09-26 15:29:31 +0800

    Initial commit: Set up main function and welcome message

---
+++ recommender.py
@@ -0,0 +1,16 @@
+# LDCW6123 - Group Project: Part 2 Interactive Program
+# Program Title: Spotify Playlist Recommendation Assistant
+
+def recommend_playlist():
+    """
+    Main function to run the playlist recommender.
+    """
+
+    # Welcome message and instructions for the user.
+    print("=====================================================")
+    print(" Welcome to the Spotify Playlist Recommendation Assistant!")
+    print("=====================================================")
+
+# This line runs the main function when the script is executed.
+if __name__ == "__main__":
+    recommend_playlist()
